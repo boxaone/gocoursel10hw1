8acd292ff8521a2e0d53956c6f7d84e66c45eeb6
Oleksii Pysarenko <boxaone@outlook.com>
2022-07-06 20:57:32 +0300

    Added birth methods and locales

--- main.go
+++ main.go
@@ -1,11 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 const (
 	catFoodPerMonth = 7
+	catMinWeight    = 1
+	catMaxWeight    = 7
 	dogFoodPerMonth = 10 / 5
+	dogMinWeight    = 1
+	dogMaxWight     = 20
 	cowFoodPerMonth = 25
+	cowMinWeight    = 10
+	cowMaxWeight    = 300
+	maxPets         = 20
+)
+
+var (
+	locale     = "ua"
+	randSource *rand.Rand
+	petNames   = map[string]map[string]string{
+		"en": {
+			"cat": "cat",
+			"dog": "dog",
+			"cow": "cow",
+		},
+		"ua": {
+			"cat": "кішка",
+			"dog": "пес",
+			"cow": "корова",
+		},
+	}
+	phrases = map[string]map[string]string{
+		"en": {
+			"whoami": "I'm a %v. I'm weighting %v kg and I need %v kg of food per month\n",
+		},
+		"ua": {
+			"whoami": "Я %v. Я важу %v кілограм і мені потрібно %v кілограм їжи в місяць\n",
+		},
+	}
 )
 
 // Common types declarations
@@ -14,39 +50,110 @@ type Eater interface {
 }
 
 type WhoAmI interface {
-	whoAmI() string
+	whoAmI()
 }
 
-type Pet struct {
-	weight int
+type ProCreator interface {
+	giveBirth(weight float64) Pet
+}
+type Animal struct {
+	weight float64
+}
+
+type HaveWeight interface {
+	Weight() float64
+}
+
+type Pet interface {
+	HaveWeight
+	Eater
+	WhoAmI
+	ProCreator
+}
+
+// Return animal weight
+
+// Custom foonction of rounding food weight, int + adding 1 extra spare kg
+func foodRound(weight float64) int {
+	return int(weight) + 1
+}
+
+// Return whoami string
+func returnWhoAmI(t string, p Pet) string {
+	return fmt.Sprintf(phrases[locale]["whoami"], petNames[locale][t], p.Weight(), p.foodNeded())
+
 }
 
 // Cat declarations
-type Cat Pet
+type Cat Animal
 
 func (cat Cat) foodNeded() int {
-	return cat.weight * catFoodPerMonth
+	return foodRound(cat.weight * catFoodPerMonth)
 }
+
 func (cat Cat) whoAmI() {
-	fmt.Printf("I'm a cat. I'm weighting %v kg and I need %v kg of food per month\n", cat.weight, cat.foodNeded())
+	fmt.Println(returnWhoAmI("cat", cat))
+}
+
+func (cat Cat) giveBirth(weight float64) Pet {
+	return Cat{weight: weight}
+}
+
+func (cat Cat) Weight() float64 {
+	return cat.weight
 }
 
 // Dog declarations
-type Dog Pet
+type Dog Animal
 
 func (dog Dog) foodNeded() int {
-	return dog.weight * dogFoodPerMonth
+	return foodRound(dog.weight * dogFoodPerMonth)
 }
+
 func (dog Dog) whoAmI() {
-	fmt.Printf("I'm a dog. I'm weighting %v kg and I need %v kg of food per month\n", dog.weight, dog.foodNeded())
+	fmt.Println(returnWhoAmI("dog", dog))
+
+}
+
+func (dog Dog) giveBirth(weight float64) Pet {
+
+	return Dog{weight: weight}
+}
+
+func (dog Dog) Weight() float64 {
+	return dog.weight
 }
 
 // Cow declarations
-type Cow Pet
+type Cow Animal
 
 func (cow Cow) foodNeded() int {
-	return cow.weight * cowFoodPerMonth
+	return foodRound(cow.weight * cowFoodPerMonth)
 }
+
 func (cow Cow) whoAmI() {
-	fmt.Printf("I'm a cow. I'm weighting %v kg and I need %v kg of food per month\n", cow.weight, cow.foodNeded())
+	fmt.Println(returnWhoAmI("cow", cow))
+}
+
+func (cow Cow) giveBirth(weight float64) Pet {
+	return Cow{weight: weight}
+}
+
+func (cow Cow) Weight() float64 {
+	return cow.weight
+}
+
+// Farm declaration
+type Farm []*Pet
+
+func main() {
+	randS := rand.NewSource(time.Now().UnixNano())
+	randSource = rand.New(randS)
+	pets := []Pet{Dog{}, Cat{}, Cow{}}
+
+	for _, p := range pets {
+		p.giveBirth(10).whoAmI()
+
+	}
+
 }
